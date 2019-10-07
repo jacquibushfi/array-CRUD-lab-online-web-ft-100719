b0b8deb59bff0967a6ff76f660211f33c1d2889d
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-07 18:25:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["earth", "wind", "fire", "water"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << "ice"
+  array
 end
 
 def add_element_to_start_of_array(array, element)
